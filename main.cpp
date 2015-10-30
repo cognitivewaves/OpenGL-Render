fa6f36fb46cf98e2ee0afae55e2fdc9326ca1206
CognitiveWaves <cognitivewaves@outlook.com>
2015-10-30 16:28:27 +0530

    Implement vertex array

--- main.cpp
+++ main.cpp
@@ -3,19 +3,60 @@
 #include <stdio.h>
 #include <math.h>   // fabs
 
-static int slices = 16;
-static int stacks = 16;
-
 static double zoom = .01;
 static int width = 0;
 static int height = 0;
 static float thetax=0, thetay=0, thetaz=0;
 
-float a[] = { 0.0, 0.0, 0.0,    // origin
-              2.0, 0.0, 0.0,    // x-axis
-              0.0, 2.0, 0.0,    // y-axis
-              0.0, 0.0, 2.0 };  // z-axis
+//      Y             Z
+//      |           /
+//      |         /
+//      |       /
+//      |     /
+//      |   /
+//      | /
+//      /--------------
+//      O              X
 
+
+float av[] = { 0.0, 0.0, 0.0,    // origin
+               2.0, 0.0, 0.0,    // x-axis
+               0.0, 2.0, 0.0,    // y-axis
+               0.0, 0.0, 2.0 };  // z-axis
+
+GLubyte avi[] = { 0, 1,
+                  0, 2,
+                  0, 3 };
+
+float ac[] = { 1.0, 0.0, 0.0,    // red   x-axis
+               0.0, 1.0, 0.0,    // green y-axis
+               0.0, 0.0, 1.0 };  // blue  z-axis
+
+GLubyte aci[] = { 0, 0,
+                  1, 1,
+                  2, 2 };
+
+float ave[6*3];
+void expandAxesVertices()
+{
+    for (int i=0; i<6; i++)
+    {
+        ave[i*3+0] = av[avi[i]*3+0];
+        ave[i*3+1] = av[avi[i]*3+1];
+        ave[i*3+2] = av[avi[i]*3+2];
+    }
+}
+
+float ace[6*3];
+void expandAxesColors()
+{
+    for (int i=0; i<6; i++)
+    {
+        ace[i*3+0] = ac[aci[i]*3+0];
+        ace[i*3+1] = ac[aci[i]*3+1];
+        ace[i*3+2] = ac[aci[i]*3+2];
+    }
+}
 
 //  (3,4,5)          (6,7,8)
 //     1----------------2
@@ -34,6 +75,50 @@ float v[] = { 1.0, 1.0, 1.0,    // 0
               2.0, 2.0, 1.0,    // 2
               2.0, 1.0, 1.0,    // 3
               1.5, 1.5, 2.0 };  // 4
+
+GLubyte pvi[] = {0, 1, 2,
+                 2, 3, 0,
+                 0, 3, 4,
+                 3, 2, 4,
+                 2, 1, 4,
+                 1, 0, 4};
+
+float pve[18*3];
+void expandVertices()
+{
+    for (int i=0; i<18; i++)
+    {
+        pve[i*3+0] = v[pvi[i]*3+0];
+        pve[i*3+1] = v[pvi[i]*3+1];
+        pve[i*3+2] = v[pvi[i]*3+2];
+    }
+}
+
+float c[] = { 1.0, 1.0, 1.0,
+              0.5, 0.5, 0.0,
+              0.0, 0.5, 0.5,
+              0.5, 0.0, 0.5,
+              0.5, 0.5, 0.5 };
+
+GLubyte pci[] = { 0, 0, 0,
+                  0, 0, 0,
+                  1, 1, 1,
+                  2, 2, 2,
+                  3, 3, 3,
+                  4, 4, 4 };
+
+float pce[18*3];
+void expandColors()
+{
+    for (int i=0; i<18; i++)
+    {
+        pce[i*3+0] = c[pci[i]*3+0];
+        pce[i*3+1] = c[pci[i]*3+1];
+        pce[i*3+2] = c[pci[i]*3+2];
+    }
+}
+
+float n[6*3*3];
 
 bool double_equal(double a, double b)
 {
@@ -106,6 +191,48 @@ static void resize(int w, int h)
     glViewport(0, 0, width, height);
 }
 
+void calculateNormals()
+{
+    for (int i=0; i<6; i++)
+    {
+        float* v0=&v[pvi[i*3+0]*3];
+        float v0x=v0[0], v0y=v0[1], v0z=v0[2];
+
+        float* v1=&v[pvi[i*3+1]*3];
+        float v1x=v1[0], v1y=v1[1], v1z=v1[2];
+
+        float* v2=&v[pvi[i*3+2]*3];
+        float v2x=v2[0], v2y=v2[1], v2z=v2[2];
+
+        float ux = v1x - v0x;
+        float uy = v1y - v0y;
+        float uz = v1z - v0z;
+
+        float vx = v2x - v0x;
+        float vy = v2y - v0y;
+        float vz = v2z - v0z;
+
+        float nx = uy*vz - uz*vy;
+        float ny = uz*vx - ux*vz;
+        float nz = ux*vy - uy*vx;
+
+        printf("\n ---------- calcNormal  ");
+        printf("%g, %g, %g\n", nx, ny, nz);
+
+        n[i*3*3+0]=nx;
+        n[i*3*3+1]=ny;
+        n[i*3*3+2]=nz;
+
+        n[i*3*3+3]=nx;
+        n[i*3*3+4]=ny;
+        n[i*3*3+5]=nz;
+
+        n[i*3*3+6]=nx;
+        n[i*3*3+7]=ny;
+        n[i*3*3+8]=nz;
+    }
+
+}
 void setNormal(float v1x, float v1y, float v1z,
                float v2x, float v2y, float v2z, 
                float v3x, float v3y, float v3z)
@@ -122,7 +249,114 @@ void setNormal(float v1x, float v1y, float v1z,
     float ny = uz*vx - ux*vz;
     float nz = ux*vy - uy*vx;
 
+    printf("\n ------- setNormal ");
+    printf("%g, %g, %g\n", nx, ny, nz);
+
     glNormal3f(nx, ny, nz);
+}
+
+static void drawVertexArray()
+{
+    expandAxesVertices();
+    expandAxesColors();
+
+    expandVertices();
+    expandColors();
+    calculateNormals();
+
+    // Draw axes
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    glVertexPointer(3, GL_FLOAT, 0, ave);
+    glColorPointer(3, GL_FLOAT, 0, ace);
+    glDrawArrays( GL_LINES, 0, 6 );
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+
+    // Draw pyramid
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glEnableClientState(GL_NORMAL_ARRAY);
+
+    glVertexPointer(3, GL_FLOAT, 0, pve);
+    glColorPointer(3, GL_FLOAT, 0, pce);
+    glNormalPointer(GL_FLOAT, 0, n);
+
+    glDrawArrays( GL_TRIANGLES, 0, 18 );
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_NORMAL_ARRAY);
+}
+
+static void drawImmediate()
+{
+    calculateNormals();
+
+    // x-axis in red
+    glColor3d(1, 0, 0);
+    glBegin(GL_LINES);
+    glVertex3f(av[0], av[1], av[2]);
+    glVertex3f(av[3], av[4], av[5]);
+    glEnd();
+
+    // y-axis in green
+    glColor3d(0, 1, 0);
+    glBegin(GL_LINES);
+    glVertex3f(av[0], av[1], av[2]);
+    glVertex3f(av[6], av[7], av[8]);
+    glEnd();
+
+    // z-axis in blue
+    glColor3d(0, 0, 1);
+    glBegin(GL_LINES);
+    glVertex3f(av[0], av[1], av[2]);
+    glVertex3f(av[9], av[10], av[11]);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glColor3d(1, 1, 1);
+        glVertex3f(v[0], v[1], v[2]);   // 0
+        glVertex3f(v[3], v[4], v[5]);   // 1
+        glVertex3f(v[6], v[7], v[8]);   // 2
+        //setNormal(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8]);
+        glNormal3f(n[3*0+0], n[3*0+1], n[3*0+2]);
+
+        glVertex3f(v[6], v[7], v[8]);   // 2
+        glVertex3f(v[9], v[10], v[11]); // 3
+        glVertex3f(v[0], v[1], v[2]);   // 0
+        glNormal3f(n[3*1+0], n[3*1+1], n[3*1+2]);
+
+        glColor3f(.5, .5, 0);
+        glVertex3f(v[0], v[1], v[2]);       // 0
+        glVertex3f(v[9], v[10], v[11]);     // 3
+        glVertex3f(v[12], v[13], v[14]);    // 4
+        //setNormal(v[0], v[1], v[2], v[9], v[10], v[11],  v[12], v[13], v[14]);
+        glNormal3f(n[3*2+0], n[3*2+1], n[3*2+2]);
+
+        glColor3f(0, .5, .5);
+        glVertex3f(v[9], v[10], v[11]);     // 3
+        glVertex3f(v[6], v[7], v[8]);       // 2
+        glVertex3f(v[12], v[13], v[14]);    // 4
+        //setNormal(v[9], v[10], v[11], v[6], v[7], v[8], v[12], v[13], v[14]);
+        glNormal3f(n[3*3+0], n[3*3+1], n[3*3+2]);
+
+        glColor3f(.5, 0, .5);
+        glVertex3f(v[6], v[7], v[8]);       // 2
+        glVertex3f(v[3], v[4], v[5]);       // 1
+        glVertex3f(v[12], v[13], v[14]);    // 4
+        //setNormal(v[6], v[7], v[8], v[3], v[4], v[5], v[12], v[13], v[14]);
+        glNormal3f(n[3*4+0], n[3*4+1], n[3*4+2]);
+
+        glColor3f(.5, .5, .5);
+        glVertex3f(v[3], v[4], v[5]);       // 1
+        glVertex3f(v[0], v[1], v[2]);       // 0
+        glVertex3f(v[12], v[13], v[14]);    // 4
+        //setNormal(v[3], v[4], v[5], v[0], v[1], v[2], v[12], v[13], v[14]);
+        glNormal3f(n[3*5+0], n[3*5+1], n[3*5+2]);
+    glEnd();
 }
 
 static void display(void)
@@ -142,61 +376,8 @@ static void display(void)
     glRotatef(thetay, 0, 1, 0);
     glRotatef(thetaz, 0, 0, 1);
 
-    // x-axis in red
-    glColor3d(1, 0, 0);
-    glBegin(GL_LINES);
-    glVertex3f(a[0], a[1], a[2]);
-    glVertex3f(a[3], a[4], a[5]);
-    glEnd();
-
-    // y-axis in green
-    glColor3d(0, 1, 0);
-    glBegin(GL_LINES);
-    glVertex3f(a[0], a[1], a[2]);
-    glVertex3f(a[6], a[7], a[8]);
-    glEnd();
-
-    // z-axis in blue
-    glColor3d(0, 0, 1);
-    glBegin(GL_LINES);
-    glVertex3f(a[0], a[1], a[2]);
-    glVertex3f(a[9], a[10], a[11]);
-    glEnd();
-
-    glBegin(GL_QUADS);
-        glColor3d(1, 1, 1);
-        glVertex3f(v[0], v[1], v[2]);   // 0
-        glVertex3f(v[3], v[4], v[5]);   // 1
-        glVertex3f(v[6], v[7], v[8]);   // 2
-        glVertex3f(v[9], v[10], v[11]); // 3
-        setNormal(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8]);
-    glEnd();
-
-    glBegin(GL_TRIANGLES);
-        glColor3f(.5, .5, 0);
-        glVertex3f(v[0], v[1], v[2]);       // 0
-        glVertex3f(v[9], v[10], v[11]);     // 3
-        glVertex3f(v[12], v[13], v[14]);    // 4
-        setNormal(v[0], v[1], v[2], v[9], v[10], v[11],  v[12], v[13], v[14]);
-
-        glColor3f(0, .5, .5);
-        glVertex3f(v[9], v[10], v[11]);     // 3
-        glVertex3f(v[6], v[7], v[8]);       // 2
-        glVertex3f(v[12], v[13], v[14]);    // 4
-        setNormal(v[9], v[10], v[11], v[6], v[7], v[8], v[12], v[13], v[14]);
-
-        glColor3f(.5, 0, .5);
-        glVertex3f(v[6], v[7], v[8]);       // 2
-        glVertex3f(v[3], v[4], v[5]);       // 1
-        glVertex3f(v[12], v[13], v[14]);    // 4
-        setNormal(v[6], v[7], v[8], v[3], v[4], v[5], v[12], v[13], v[14]);
-
-        glColor3f(.5, .5, .5);
-        glVertex3f(v[3], v[4], v[5]);       // 1
-        glVertex3f(v[0], v[1], v[2]);       // 0
-        glVertex3f(v[12], v[13], v[14]);    // 4
-        setNormal(v[3], v[4], v[5], v[0], v[1], v[2], v[12], v[13], v[14]);
-    glEnd();
+    drawImmediate();
+    //drawVertexArray();
 
     glutSwapBuffers();
 }
